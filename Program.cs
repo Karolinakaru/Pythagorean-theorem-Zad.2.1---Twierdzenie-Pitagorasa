34912c57fb94186e3631a80eb232fdd8fc46f09e
singhet <singhet@SCSWL4072>
2024-01-15 19:12:28 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a = 3.5, b= 6, c;
+        c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        Console.Write("In rectangular triangle with a = 3.5 and b = 6, the length of the hypotenuse c is: " + c);
+        Console.ReadKey();
+    }
+}
